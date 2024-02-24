1d623dfbd6579bf6c30d5146fbd3596adf10e0ac
muasya-victor <vicmwe184@gmail.com>
2024-02-24 19:58:00 +0300

    - bugfix  - reconfigure for windows

--- logic/registerProperty.php
+++ logic/registerProperty.php
@@ -12,6 +12,26 @@ if(isset($_POST['submit'])){
     $avatar = $_POST['avatar'];
     $rented = false;
 
+    if (isset($_FILES['avatar']) && $_FILES['avatar']['error'] === UPLOAD_ERR_OK) {
+        // Specify the folder to move the avatar to
+        $target_directory = "projectMedia/";
+
+        // Extract the filename from the avatar path
+        $avatar_name = basename($_FILES["avatar"]["name"]);
+
+        // Set the destination path
+        $destination = $target_directory . $avatar_name;
+
+        // Move the uploaded file to the destination folder
+        if (move_uploaded_file($_FILES["avatar"]["tmp_name"], $destination)) {
+            echo "The file ". $avatar_name. " has been uploaded.";
+        } else {
+            echo "Sorry, there was an error moving the uploaded file.";
+        }
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+
     $query = "INSERT INTO property(house_type,price,location,avatar,rented)";
     $query .= "VALUES('{$house_type}', $price, '{$location}', '{$avatar}','{$rented}')";
 
